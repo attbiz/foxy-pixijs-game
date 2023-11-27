c024ce8b5e64a1535bcbf7a88f81b243a023de9d
A <a@localhost>
2023-11-27 23:53:39 +0000

    add github link

--- +page.svelte
+++ +page.svelte
@@ -25,7 +25,15 @@
 </script>
 
 <div class="p-8 min-h-[75vh] space-y-8 max-w-screen-md mx-auto">
-	<h2 class="h2 text-center">Foxy: Protect the House</h2>
+	<header class="space-y-2">
+		<h2 class="h2 text-center">Foxy: Protect the House</h2>
+		<p class="text-center">
+			<a href="https://github.com/attbiz/foxy-pixijs-game" target="_blank">
+				<i class="fa-brands fa-github" />
+				<u>GitHub</u>
+			</a>
+		</p>
+	</header>
 	<div bind:this={container} class="mx-auto max-w-min rounded-lg" />
 	<section class="text-center">
 		<p>Your goal is to protect the house and collect gems.</p>
